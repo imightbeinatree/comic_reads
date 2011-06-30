f302ee2908e1e852be9e18bf8d91466304bed628
Michael Orr <michael@cloudspace.com>
2011-06-30 14:43:09 -0400

    new project with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComicReads::Application.config.secret_token = '2ced99975ff7c6cf318a815e0145bc29b1a25b21c0fa375c81820ac35a9f594ec29755baa1ef76b3e2c28332066b25f51d0ad2d6f4a95438a6c14e269e828e9e'
